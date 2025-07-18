f4950587b1cc941d52d0cb26a0186b280bb5e7bd
Tomek <tomek.wojak796@gmail.com>
2025-07-19 01:02:45 +0200

    finished js funtionality and header

--- js/script.js
+++ js/script.js
@@ -3,15 +3,29 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	const changeMode = () => {
 		modeToggler.classList.toggle("active");
+		let mode;
 
 		if (modeToggler.classList.contains("active")) {
 			modeToggler.setAttribute("aria-label", "Turn on light mode");
 			document.body.classList.add("dark");
+			mode = "dark";
 		} else {
 			modeToggler.setAttribute("aria-label", "Turn on dark mode");
 			document.body.classList.remove("dark");
+			mode = "light";
 		}
+		localStorage.setItem("theme", mode);
 	};
 
 	modeToggler.addEventListener("click", changeMode);
+	window.addEventListener("DOMContentLoaded", () => {
+		const theme = localStorage.getItem("theme");
+		if (theme === "dark") {
+			document.body.classList.add("dark");
+			modeToggler.classList.add("active");
+		} else {
+			document.body.classList.remove("dark");
+			modeToggler.classList.remove("active");
+		}
+	});
 });
